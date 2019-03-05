f380ac58ca1137361a4a5d263869c109654fc5ee
anoopks1207 <42033222+anoopks1207@users.noreply.github.com>
2019-03-04 19:01:31 -0600

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -4,6 +4,5 @@ $dbhost = getenv("MYSQL_SERVICE_HOST");
 $dbuser = getenv("database-user");
 $dbpasswd = getenv("database-password");
 $dbname = getenv("database-name");
-mysql_connect($dbhost, $dbuser, $dbpasswd);
-mysql_select_db($dbname);
+$con = mysqli_connect($dbhost, $dbuser, $dbpasswd, $dbname);
 ?>
