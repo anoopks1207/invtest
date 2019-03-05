23627ac2a94bbeda43c27464d4464d56e09990cd
anoopks1207 <42033222+anoopks1207@users.noreply.github.com>
2019-03-04 18:39:16 -0600

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,9 @@
 <?php
 error_reporting(E_ALL ^ E_DEPRECATED);
+$dbhost = getenv("MYSQL_SERVICE_HOST")
 $dbuser = getenv("database-user");
 $dbpasswd = getenv("database-password");
 $dbname = getenv("database-name");
-mysql_connect("localhost","$dbuser","$dbpasswd");
+mysql_connect("$dbhost","$dbuser","$dbpasswd");
 mysql_select_db("$dbname");
 ?>
